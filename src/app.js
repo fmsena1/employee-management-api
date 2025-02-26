0d61d7aec8e70ba83d601cdac9270884a54c526d
Filipe Magalhães Sena <94627887+fmsena1@users.noreply.github.com>
2025-02-25 23:45:35 -0300

    feat: enhance CORS configuration to allow specific headers and origins

--- src/app.js
+++ src/app.js
@@ -21,9 +21,18 @@ const corsOptions = {
   },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.json());
 app.use("/", routes);
 
